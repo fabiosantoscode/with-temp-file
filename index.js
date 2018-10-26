409debafe69b8e1d87035b62271a23837fe9a602
Fábio Santos <fabiosantosart@gmail.com>
2018-10-26 23:20:02 +0100

    delay file unlink

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var userAsyncFunction = require('user-async-function')
 module.exports = function withTempFile (fn, filename) {
   if (!filename) filename = tempfile()
   return userAsyncFunction(fn, fs.createWriteStream(filename), filename).then(function (ret) {
-    fs.unlink(filename, function () {})
+    setTimeout(function () { fs.unlink(filename, function () {}) }, 300)
     return ret
   })
 }
